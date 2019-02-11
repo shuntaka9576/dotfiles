d3a9150f1bc2597ef5c1f69153ffb914b353dfc5
shuntaka9576 <shuntaka9576@gmail.com>
2019-02-11 21:19:29 +0900

    Add alias for  fazzy repository move

--- fish/actions/alias.fish
+++ fish/actions/alias.fish
@@ -9,15 +9,25 @@ alias ga='git add'
 alias gl='git log' 
 alias gr='git reset --hard' 
 
-# ghq alias
+# simple repository move
 alias g='cd (ghq root)/(ghq list | peco)'
 
-# nvim
+# nvim alias
 alias nv='nvim'
+
+# fazzy repository move
+function fg
+    cd (ghq list --full-path |fzf-tmux)
+end
+
+# fazzy cd
+function fd
+    cd (find . -type d |fzf-tmux)
+end
 
 # ls after cd
 functions --copy cd standard_cd
-
 function cd
-  standard_cd $argv; and ls
+    standard_cd $argv; and ls
 end
+
