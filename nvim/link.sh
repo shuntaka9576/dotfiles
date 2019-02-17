b79079ee36b162f559e8d92969a22003b170d3bb
shuntaka9576 <shuntaka9576@gmail.com>
2019-02-17 13:49:39 +0900

    Change the directory of the toml files for neovim

--- nvim/link.sh
+++ nvim/link.sh
@@ -2,10 +2,10 @@
 
 mkdir -p ~/.config/nvim/dein
 ln -sf ~/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
-ln -sf ~/dotfiles/nvim/notlazy.toml ~/.config/nvim/dein/notlazy.toml
-ln -sf ~/dotfiles/nvim/frontend.toml ~/.config/nvim/dein/frontend.toml
-ln -sf ~/dotfiles/nvim/extentions.toml ~/.config/nvim/dein/extentions.toml
-ln -sf ~/dotfiles/nvim/git.toml ~/.config/nvim/dein/git.toml
-ln -sf ~/dotfiles/nvim/go.toml ~/.config/nvim/dein/go.toml
-ln -sf ~/dotfiles/nvim/hack.toml ~/.config/nvim/dein/hack.toml
-ln -sf ~/dotfiles/nvim/utils.toml ~/.config/nvim/dein/utils.toml
+ln -sf ~/dotfiles/nvim/units/notlazy.toml ~/.config/nvim/dein/notlazy.toml
+ln -sf ~/dotfiles/nvim/units/frontend.toml ~/.config/nvim/dein/frontend.toml
+ln -sf ~/dotfiles/nvim/units/extentions.toml ~/.config/nvim/dein/extentions.toml
+ln -sf ~/dotfiles/nvim/units/git.toml ~/.config/nvim/dein/git.toml
+ln -sf ~/dotfiles/nvim/units/go.toml ~/.config/nvim/dein/go.toml
+ln -sf ~/dotfiles/nvim/units/hack.toml ~/.config/nvim/dein/hack.toml
+ln -sf ~/dotfiles/nvim/units/utils.toml ~/.config/nvim/dein/utils.toml
