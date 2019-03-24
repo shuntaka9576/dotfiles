5e251016617e54f3d7ece452f38d70f4fafa1557
shuntaka9576 <shuntaka9576@gmail.com>
2019-03-24 22:55:33 +0900

    Add language-servers setting

--- init/setup/frontend.sh
+++ init/setup/frontend.sh
@@ -3,6 +3,9 @@
 brew install npm
 npm install -g vue-language-server
 npm install -g javascript-typescript-langserver
+npm install -g dockerfile-language-server-nodejs
+npm install -g bash-language-server
+npm install -g markdownlint
 npm install -g typescript
 npm install -g yarn
 npm install -g prettier
--- nvim/link.sh
+++ nvim/link.sh
@@ -10,5 +10,9 @@ ln -sf ~/dotfiles/nvim/units/go.toml ~/.config/nvim/dein/go.toml
 ln -sf ~/dotfiles/nvim/units/hack.toml ~/.config/nvim/dein/hack.toml
 ln -sf ~/dotfiles/nvim/units/utils.toml ~/.config/nvim/dein/utils.toml
 ln -sf ~/dotfiles/nvim/coc-settings.json ~/.config/nvim/coc-settings.json
+
 mkdir -p ~/.config/coc/extensions
 ln -sf ~/dotfiles/nvim/package.json ~/.config/coc/extensions/package.json
+
+mkdir -p ~/.config/efm-langserver
+ln -sf ~/dotfiles/nvim/config.yaml ~/.config/efm-langserver/config.yaml
