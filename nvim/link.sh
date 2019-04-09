c832b6a7e29765161fda2dec410757b0cfb54fd0
shuntaka9576 <shuntaka9576@gmail.com>
2019-04-09 22:34:38 +0900

    Add coc settings

--- nvim/link.sh
+++ nvim/link.sh
@@ -7,12 +7,14 @@ ln -sf ~/dotfiles/nvim/units/frontend.toml ~/.config/nvim/dein/frontend.toml
 ln -sf ~/dotfiles/nvim/units/extentions.toml ~/.config/nvim/dein/extentions.toml
 ln -sf ~/dotfiles/nvim/units/git.toml ~/.config/nvim/dein/git.toml
 ln -sf ~/dotfiles/nvim/units/go.toml ~/.config/nvim/dein/go.toml
+ln -sf ~/dotfiles/nvim/units/python.toml ~/.config/nvim/dein/python.toml
 ln -sf ~/dotfiles/nvim/units/hack.toml ~/.config/nvim/dein/hack.toml
 ln -sf ~/dotfiles/nvim/units/utils.toml ~/.config/nvim/dein/utils.toml
-ln -sf ~/dotfiles/nvim/coc-settings.json ~/.config/nvim/coc-settings.json
 
+# coc settings
 mkdir -p ~/.config/coc/extensions
-ln -sf ~/dotfiles/nvim/package.json ~/.config/coc/extensions/package.json
-
 mkdir -p ~/.config/efm-langserver
-ln -sf ~/dotfiles/nvim/config.yaml ~/.config/efm-langserver/config.yaml
+ln -sf ~/dotfiles/nvim/coc/pycodestyle.json ~/.config/pycodestyle
+ln -sf ~/dotfiles/nvim/coc/coc-settings.json ~/.config/nvim/coc-settings.json
+ln -sf ~/dotfiles/nvim/coc/package.json ~/.config/coc/extensions/package.json
+ln -sf ~/dotfiles/nvim/coc/config.yaml ~/.config/efm-langserver/config.yaml
