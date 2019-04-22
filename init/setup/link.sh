0e75fa74d1b54f03b542292e5b96c589ab26b459
shuntaka9576 <shuntaka9576@gmail.com>
2019-04-22 12:57:10 +0900

    Add tig settigs

--- init/setup/link.sh
+++ init/setup/link.sh
@@ -5,6 +5,8 @@ mkdir -p ~/.config/fish
 
 # link dotfiles
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/.tigrc  ~/.tigrc
+
 ln -sf ~/dotfiles/fish/config.fish  ~/.config/fish/config.fish
 if [ "$(uname)" == 'Darwin' ]; then
   ln -sf ~/dotfiles/.bashrc ~/.bash_profile
