801b5d3e758b4c70866db24782d1d8cfc36dfa7c
shuntaka9576 <shuntaka9576@gmail.com>
2019-05-03 01:57:57 +0900

    Add fisher plugin

--- fish/fish_plug.sh
+++ fish/fish_plug.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-fish -C 'fisher add oh-my-fish/theme-bobthefish;fisher add jethrokuan/z;fisher add jethrokuan/fzf;fisher add decors/fish-ghq;'
+fish -C 'fisher add oh-my-fish/theme-bobthefish;fisher add jethrokuan/z;fisher add jethrokuan/fzf;fisher add decors/fish-ghq;fisher add rafaelrinaldi/pure;fisher add fish-bd;'
