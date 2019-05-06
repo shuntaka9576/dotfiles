7fdd41fbbfe8d4397df258b62481490dce771e60
shuntaka9576 <shuntaka9576@gmail.com>
2019-05-06 23:06:08 +0900

    Add vscode config

---
+++ vscode/link.bat
@@ -0,0 +1,5 @@
+mklink %APPDATA%\Code\User\keybindings.json C:\dotfiles\vscode\keybindings.json
+mklink %APPDATA%\Code\User\settings.json C:\dotfiles\vscode\settings.json
+
+mklink %APPDATA%\"Code - Insiders"\User\keybindings.json C:\dotfiles\vscode\keybindings.json
+mklink %APPDATA%\"Code - Insiders"\User\settings.json C:\dotfiles\vscode\settings.json
