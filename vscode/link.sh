0836c22c2e70e4f9e6ad8e79f566ebd5baef0dbe
shuntaka9576 <shuntaka9576@gmail.com>
2019-05-06 21:18:35 +0900

    Add vscode config

---
+++ vscode/link.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mkdir -p ~/Library/Application\ Support/Code/User
+
+ln -sf ~/dotfiles/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
+ln -sf ~/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
