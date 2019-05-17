86636bc6ae0c1d78ebe6b2ad27312746b3b7da68
shuntaka9576 <shuntaka9576@gmail.com>
2019-05-17 19:03:22 +0900

    Adjustment pet config

--- tools/link.sh
+++ tools/link.sh
@@ -2,5 +2,7 @@
 
 mkdir -p ~/.config/memo
 ln -sf ~/dotfiles/tools/memo/config.toml ~/.config/memo/config.toml
+
+mkdir -p ~/.config/pet
 ln -sf ~/dotfiles/tools/pet/config.toml ~/.config/pet/config.toml
 ln -sf ~/dotfiles/tools/pet/snippet.toml ~/.config/pet/snippet.toml
