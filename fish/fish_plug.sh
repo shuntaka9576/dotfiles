ac5ecb28525cfe08a3607dca8eb35559773603d1
shuntaka9576 <shuntaka9576@gmail.com>
2019-06-24 00:43:56 +0900

    Change fish theme

--- fish/fish_plug.sh
+++ fish/fish_plug.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-fish -C 'fisher add oh-my-fish/theme-bobthefish;fisher add jethrokuan/z;fisher add jethrokuan/fzf;fisher add decors/fish-ghq;fisher add rafaelrinaldi/pure;fisher add fish-bd;fisher add otms61/fish-pet;'
+fish -C 'fisher add oh-my-fish/theme-bobthefish;fisher add jethrokuan/z;fisher add jethrokuan/fzf;fisher add decors/fish-ghq;fisher add rafaelrinaldi/pure;fisher add fish-bd;fisher add otms61/fish-pet;fisher add matchai/spacefish;'
