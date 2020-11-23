ce189d12ffe0f3fb9be05c8d018730fb78ef534d
shuntaka9576 <shuntaka9576@gmail.com>
2020-11-23 12:55:53 +0900

    Split install go cli script

--- init/setup/common.sh
+++ init/setup/common.sh
@@ -22,21 +22,8 @@ curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh >
 sh ~/installer.sh ~/.cache/dein
 rm ~/installer.sh
 
-# install go binaries
-export GOPATH=~/go
-go get github.com/x-motemen/ghq
-go get github.com/mrtazz/checkmake
-go get github.com/mattn/memo
-go get github.com/jesseduffield/lazygit
-go get github.com/knqyf263/pet
-go get github.com/mvdan/sh/cmd/shfmt
-go get github.com/github/hub
-# go get github.com/gohugoio/hugo
-# go get github.com/mattn/efm-langserver/cmd/efm-langserver
-
-go get -d github.com/skanehira/docui
-cd $GOPATH/src/github.com/skanehira/docui
-GO111MODULE=on go install
+# install go binary(unstable)
+~/dotfiles/init/setup/go.sh
 
 # install pip3 packages
 sudo pip3 install python-language-server
