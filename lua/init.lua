1c0cd5fde0db0a0e570985fa9b7a9ab9bf773a77
shuntaka9576 <shuntaka9576@gmail.com>
2021-10-30 20:08:48 +0900

    Adjustment toggleterm

--- lua/init.lua
+++ lua/init.lua
@@ -197,7 +197,7 @@ require('packer').startup(function()
       local Terminal = require('toggleterm.terminal').Terminal
       local lazygit = Terminal:new({cmd = "lazygit", hidden = true})
 
-      local function _lazygit_toggle() lazygit:toggle() end
+      function _lazygit_toggle() lazygit:toggle() end
 
       vim.api.nvim_set_keymap("n", "<leader>g",
                               "<cmd>lua _lazygit_toggle()<CR>",
