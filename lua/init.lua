aa56cd605f3e3cd3790c24c31435d5e94a589a22
shuntaka9576 <shuntaka9576@gmail.com>
2021-10-31 14:17:19 +0900

    Refactoring

--- lua/init.lua
+++ lua/init.lua
@@ -243,15 +243,16 @@ cmp.setup.cmdline(':', {
 ----------------------------
 -- nvim-lspconfig settings
 ----------------------------
-local on_attach = function(_, bufnr)
+local nvim_lsp = require('lspconfig')
+
+local on_attach = function(client, bufnr)
   local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
   local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
 
   buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-  local opts = {noremap = true, silent = true}
+  local opts = {noremap = true, silent = false}
 
-  -- buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
   buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
   buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
   buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
@@ -263,35 +264,24 @@ local on_attach = function(_, bufnr)
   buf_set_keymap('n', '<space>wl',
                  '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>',
                  opts)
-  -- buf_set_keymap('n', '<space>D',
-  --             '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
   buf_set_keymap('n', 'rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  -- buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>',
-  -- opts)
-  -- buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  -- buf_set_keymap('n', '<space>e',
-  --                '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>',
-  --                opts)
   buf_set_keymap('n', '<C-n>', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>',
                  opts)
-  -- buf_set_keymap('n', '<C-N>', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>',
-  -- opts)
   buf_set_keymap('n', '<space>q',
                  '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
   buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
 end
 
-local nvim_lsp = require('lspconfig')
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
-
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp
+                                                                   .protocol
+                                                                   .make_client_capabilities())
 local servers = {'sumneko_lua'}
 for _, lsp in ipairs(servers) do
-  nvim_lsp[lsp].setup {
+  nvim_lsp[lsp].setup({
     on_attach = on_attach,
     flags = {debounce_text_changes = 150},
     capabilities = capabilities
-  }
+  })
 end
 
 ----------------------------
