5fcd769eda4c7d9a18961b0780f179a0fedb7375
shuntaka9576 <shuntaka9576@gmail.com>
2022-04-16 08:25:31 +0900

    Support nvim-treesitter latest version

--- lua/init.lua
+++ lua/init.lua
@@ -222,7 +222,7 @@ packer.startup(function(use)
     run = ":TSUpdate",
     config = function()
       require("nvim-treesitter.configs").setup({
-        ensure_installed = "maintained",
+        ensure_installed = "all",
         highlight = { enable = true },
         -- additional_vim_regex_highlighting = true
       })
