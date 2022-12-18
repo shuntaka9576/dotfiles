e36b55bae3c33f0c586b0d28b7f623a29cdaaeff
shuntaka9576 <shuntaka9576@gmail.com>
2022-12-18 16:23:07 +0900

    Adjustment completion settings

--- lua/init.lua
+++ lua/init.lua
@@ -135,10 +135,14 @@ packer.startup(function(use)
               \ coc#refresh()
         inoremap <expr><S-TAB> coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"
         " Make <CR> to accept selected completion item or notify coc.nvim to format
-        " <C-g>u breaks current undo, please make your own choice.
-        " inoremap <silent><expr> <CR> coc#pum#visible() ? coc#pum#confirm()
-        "                       \: "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"
-        inoremap <silent><expr> <CR> coc#pum#visible() ? coc#pum#confirm() : "\<CR>"
+        " <C-g>u breaks current undo, please make your own choice
+        inoremap <silent><expr> <CR> coc#pum#visible() ? coc#pum#confirm()
+                                      \: "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"
+        function! CheckBackspace() abort
+          let col = col('.') - 1
+          return !col || getline('.')[col - 1]  =~# '\s'
+        endfunction
+
         " リファクタリング機能
         nmap <silent>gr <Plug>(coc-rename)
         " コードジャンプ
