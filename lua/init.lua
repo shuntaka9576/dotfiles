ee40d8353a79033f9311011814d1859aa876fcb7
shuntaka9576 <shuntaka9576@gmail.com>
2023-04-25 06:28:51 +0900

    Delete nvim-tree legacy keymap

--- lua/init.lua
+++ lua/init.lua
@@ -209,19 +209,81 @@ require("lazy").setup({
     opt = true,
     event = { "VimEnter" },
     config = function()
+      local function on_attach(bufnr)
+        local api = require('nvim-tree.api')
+
+        local function opts(desc)
+          return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+        end
+
+        -- BEGIN_DEFAULT_ON_ATTACH
+        vim.keymap.set('n', '<C-]>', api.tree.change_root_to_node, opts('CD'))
+        vim.keymap.set('n', '<C-e>', api.node.open.replace_tree_buffer, opts('Open: In Place'))
+        vim.keymap.set('n', '<C-k>', api.node.show_info_popup, opts('Info'))
+        vim.keymap.set('n', '<C-r>', api.fs.rename_sub, opts('Rename: Omit Filename'))
+        vim.keymap.set('n', '<C-t>', api.node.open.tab, opts('Open: New Tab'))
+        vim.keymap.set('n', '<C-v>', api.node.open.vertical, opts('Open: Vertical Split'))
+        vim.keymap.set('n', '<C-x>', api.node.open.horizontal, opts('Open: Horizontal Split'))
+        vim.keymap.set('n', '<BS>', api.node.navigate.parent_close, opts('Close Directory'))
+        vim.keymap.set('n', '<CR>', api.node.open.edit, opts('Open'))
+        vim.keymap.set('n', '<Tab>', api.node.open.preview, opts('Open Preview'))
+        vim.keymap.set('n', '>', api.node.navigate.sibling.next, opts('Next Sibling'))
+        vim.keymap.set('n', '<', api.node.navigate.sibling.prev, opts('Previous Sibling'))
+        vim.keymap.set('n', '.', api.node.run.cmd, opts('Run Command'))
+        vim.keymap.set('n', '-', api.tree.change_root_to_parent, opts('Up'))
+        vim.keymap.set('n', 'a', api.fs.create, opts('Create'))
+        vim.keymap.set('n', 'bmv', api.marks.bulk.move, opts('Move Bookmarked'))
+        vim.keymap.set('n', 'B', api.tree.toggle_no_buffer_filter, opts('Toggle No Buffer'))
+        vim.keymap.set('n', 'c', api.fs.copy.node, opts('Copy'))
+        vim.keymap.set('n', 'C', api.tree.toggle_git_clean_filter, opts('Toggle Git Clean'))
+        vim.keymap.set('n', '[c', api.node.navigate.git.prev, opts('Prev Git'))
+        vim.keymap.set('n', ']c', api.node.navigate.git.next, opts('Next Git'))
+        vim.keymap.set('n', 'd', api.fs.remove, opts('Delete'))
+        vim.keymap.set('n', 'D', api.fs.trash, opts('Trash'))
+        vim.keymap.set('n', 'E', api.tree.expand_all, opts('Expand All'))
+        vim.keymap.set('n', 'e', api.fs.rename_basename, opts('Rename: Basename'))
+        vim.keymap.set('n', ']e', api.node.navigate.diagnostics.next, opts('Next Diagnostic'))
+        vim.keymap.set('n', '[e', api.node.navigate.diagnostics.prev, opts('Prev Diagnostic'))
+        vim.keymap.set('n', 'F', api.live_filter.clear, opts('Clean Filter'))
+        vim.keymap.set('n', 'f', api.live_filter.start, opts('Filter'))
+        vim.keymap.set('n', 'g?', api.tree.toggle_help, opts('Help'))
+        vim.keymap.set('n', 'gy', api.fs.copy.absolute_path, opts('Copy Absolute Path'))
+        vim.keymap.set('n', 'H', api.tree.toggle_hidden_filter, opts('Toggle Dotfiles'))
+        vim.keymap.set('n', 'I', api.tree.toggle_gitignore_filter, opts('Toggle Git Ignore'))
+        vim.keymap.set('n', 'J', api.node.navigate.sibling.last, opts('Last Sibling'))
+        vim.keymap.set('n', 'K', api.node.navigate.sibling.first, opts('First Sibling'))
+        vim.keymap.set('n', 'm', api.marks.toggle, opts('Toggle Bookmark'))
+        vim.keymap.set('n', 'o', api.node.open.edit, opts('Open'))
+        vim.keymap.set('n', 'O', api.node.open.no_window_picker, opts('Open: No Window Picker'))
+        vim.keymap.set('n', 'p', api.fs.paste, opts('Paste'))
+        vim.keymap.set('n', 'P', api.node.navigate.parent, opts('Parent Directory'))
+        vim.keymap.set('n', 'q', api.tree.close, opts('Close'))
+        vim.keymap.set('n', 'r', api.fs.rename, opts('Rename'))
+        vim.keymap.set('n', 'R', api.tree.reload, opts('Refresh'))
+        vim.keymap.set('n', 's', api.node.run.system, opts('Run System'))
+        vim.keymap.set('n', 'S', api.tree.search_node, opts('Search'))
+        vim.keymap.set('n', 'U', api.tree.toggle_custom_filter, opts('Toggle Hidden'))
+        vim.keymap.set('n', 'W', api.tree.collapse_all, opts('Collapse'))
+        vim.keymap.set('n', 'x', api.fs.cut, opts('Cut'))
+        vim.keymap.set('n', 'y', api.fs.copy.filename, opts('Copy Name'))
+        vim.keymap.set('n', 'Y', api.fs.copy.relative_path, opts('Copy Relative Path'))
+        vim.keymap.set('n', '<2-LeftMouse>', api.node.open.edit, opts('Open'))
+        vim.keymap.set('n', '<2-RightMouse>', api.tree.change_root_to_node, opts('CD'))
+        -- END_DEFAULT_ON_ATTACH
+
+        -- Mappings migrated from view.mappings.list
+        vim.keymap.set('n', 'A', api.tree.expand_all, opts('Expand All'))
+        vim.keymap.set('n', '?', api.tree.toggle_help, opts('Help'))
+        vim.keymap.set('n', 'C', api.tree.change_root_to_node, opts('CD'))
+
+        vim.keymap.set('n', 'Z', api.node.run.system, opts('Run System'))
+      end
+
       local nvim_tree = require("nvim-tree")
       nvim_tree.setup({
-        vim.api.nvim_set_keymap("n", "<Leader>d", ":NvimTreeToggle<CR>", { noremap = true, silent = true }),
-        view = {
-          mappings = {
-            list = {
-              { key = "<C-e>", action = "" },
-            },
-          },
-        },
+        on_attach = on_attach,
       })
-      vim.g.nvim_tree_refresh_wait = 100
-      -- nvim_tree.open()
+      vim.api.nvim_set_keymap("n", "<Leader>d", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
     end,
   },
 
@@ -323,53 +385,53 @@ require("lazy").setup({
   { "simeji/winresizer" },
 
   { "kyazdani42/nvim-web-devicons" },
-  {
-    "akinsho/bufferline.nvim",
-    dependencies = "kyazdani42/nvim-web-devicons",
-    config = function()
-      vim.opt.termguicolors = true
+  -- {
+  --   "akinsho/bufferline.nvim",
+  --   dependencies = "kyazdani42/nvim-web-devicons",
+  --   config = function()
+  --     vim.opt.termguicolors = true
 
-      local groups = require("bufferline.groups")
+  --     local groups = require("bufferline.groups")
 
-      require("bufferline").setup({
-        options = {
-          close_command = "bdelete! %d",
-          diagnostics = "coc",
-          diagnostics_indicator = function(count, level, _, _)
-            local icon = level:match("error") and " " or " "
-            return " " .. icon .. count
-          end,
-          offsets = { { filetype = "NvimTree" } },
-          groups = {
-            options = {
-              toggle_hidden_on_enter = true, -- when you re-enter a hidden group this options re-opens that group so the buffer is visible
-            },
-            items = {
-              groups.builtin.ungrouped,
-              {
-                name = "Docs",
-                auto_close = true,
-                matcher = function(buf)
-                  return buf.filename:match("%.md")
-                end,
-              },
-            },
-          },
-        },
-      })
+  --     require("bufferline").setup({
+  --       options = {
+  --         close_command = "bdelete! %d",
+  --         diagnostics = "coc",
+  --         diagnostics_indicator = function(count, level, _, _)
+  --           local icon = level:match("error") and " " or " "
+  --           return " " .. icon .. count
+  --         end,
+  --         offsets = { { filetype = "NvimTree" } },
+  --         groups = {
+  --           options = {
+  --             toggle_hidden_on_enter = true, -- when you re-enter a hidden group this options re-opens that group so the buffer is visible
+  --           },
+  --           items = {
+  --             groups.builtin.ungrouped,
+  --             {
+  --               name = "Docs",
+  --               auto_close = true,
+  --               matcher = function(buf)
+  --                 return buf.filename:match("%.md")
+  --               end,
+  --             },
+  --           },
+  --         },
+  --       },
+  --     })
 
-      vim.api.nvim_set_keymap("n", "[b", "<cmd>BufferLineCycleNext<cr>", { noremap = true, silent = true })
-      vim.api.nvim_set_keymap("n", "b]", "<cmd>BufferLineCyclePrev<cr>", { noremap = true, silent = true })
-      vim.api.nvim_set_keymap("n", "<C-s>", "<cmd>BufferLineSortByTabs<cr>", { noremap = true, silent = true })
-      vim.api.nvim_set_keymap("n", "<leader>t", "<cmd>BufferLinePick<cr>", { noremap = true, silent = true })
-      vim.api.nvim_set_keymap(
-        "n",
-        "<leader><leader>r",
-        "<cmd>BufferLineCloseRight<cr>",
-        { noremap = true, silent = true }
-      )
-    end,
-  },
+  --     vim.api.nvim_set_keymap("n", "[b", "<cmd>BufferLineCycleNext<cr>", { noremap = true, silent = true })
+  --     vim.api.nvim_set_keymap("n", "b]", "<cmd>BufferLineCyclePrev<cr>", { noremap = true, silent = true })
+  --     vim.api.nvim_set_keymap("n", "<C-s>", "<cmd>BufferLineSortByTabs<cr>", { noremap = true, silent = true })
+  --     vim.api.nvim_set_keymap("n", "<leader>t", "<cmd>BufferLinePick<cr>", { noremap = true, silent = true })
+  --     vim.api.nvim_set_keymap(
+  --       "n",
+  --       "<leader><leader>r",
+  --       "<cmd>BufferLineCloseRight<cr>",
+  --       { noremap = true, silent = true }
+  --     )
+  --   end,
+  -- },
 
   {
     "nvim-lualine/lualine.nvim",
