014ec353be0630f0763ad8aa5546ff1ed6c35670
shuntaka9576 <shuntaka9576@gmail.com>
2023-08-03 10:27:30 +0900

    Replace to flash.nvim from hop.nvim

--- lua/init.lua
+++ lua/init.lua
@@ -306,13 +306,38 @@ require("lazy").setup({
   --   end,
   -- },
 
+  -- easy motion
+  -- {
+  --   "phaazon/hop.nvim",
+  --   name = "hop",
+  --   config = function()
+  --     require("hop").setup({ keys = "etovxqpdygfblzhckisuran" })
+  --     vim.api.nvim_set_keymap("n", "f", ":HopChar1<CR>", { noremap = true, silent = true })
+  --   end,
+  -- },
   {
-    "phaazon/hop.nvim",
-    name = "hop",
-    config = function()
-      require("hop").setup({ keys = "etovxqpdygfblzhckisuran" })
-      vim.api.nvim_set_keymap("n", "f", ":HopChar1<CR>", { noremap = true, silent = true })
-    end,
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    opts = {},
+    keys = {
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end,       desc = "Flash" },
+      { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "r", mode = "o",               function() require("flash").remote() end,     desc = "Remote Flash" },
+      {
+        "R",
+        mode = { "o", "x" },
+        function() require("flash").treesitter_search() end,
+        desc =
+        "Treesitter Search"
+      },
+      {
+        "<c-s>",
+        mode = { "c" },
+        function() require("flash").toggle() end,
+        desc =
+        "Toggle Flash Search"
+      },
+    },
   },
 
   {
