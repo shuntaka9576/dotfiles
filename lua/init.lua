eea7900f74d493c37633c50e77e608ca66dcc169
shuntaka9576 <shuntaka9576@gmail.com>
2023-08-03 10:57:04 +0900

    Adjustment easymotion like plugins

--- lua/init.lua
+++ lua/init.lua
@@ -315,29 +315,44 @@ require("lazy").setup({
   --     vim.api.nvim_set_keymap("n", "f", ":HopChar1<CR>", { noremap = true, silent = true })
   --   end,
   -- },
+  -- {
+  --   "folke/flash.nvim",
+  --   event = "VeryLazy",
+  --   opts = {},
+  --   config = function()
+  --     require("flash").jump {
+  --       mode = vim.fn["kensaku#query"]
+  --     }
+  --   end,
+  --   keys = {
+  --     { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end,       desc = "Flash" },
+  --     { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+  --     { "r", mode = "o",               function() require("flash").remote() end,     desc = "Remote Flash" },
+  --     {
+  --       "R",
+  --       mode = { "o", "x" },
+  --       function() require("flash").treesitter_search() end,
+  --       desc =
+  --       "Treesitter Search"
+  --     },
+  --     {
+  --       "<c-s>",
+  --       mode = { "c" },
+  --       function() require("flash").toggle() end,
+  --       desc =
+  --       "Toggle Flash Search"
+  --     },
+  --   },
+  -- },
   {
-    "folke/flash.nvim",
-    event = "VeryLazy",
-    opts = {},
-    keys = {
-      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end,       desc = "Flash" },
-      { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-      { "r", mode = "o",               function() require("flash").remote() end,     desc = "Remote Flash" },
-      {
-        "R",
-        mode = { "o", "x" },
-        function() require("flash").treesitter_search() end,
-        desc =
-        "Treesitter Search"
-      },
-      {
-        "<c-s>",
-        mode = { "c" },
-        function() require("flash").toggle() end,
-        desc =
-        "Toggle Flash Search"
-      },
-    },
+    "yuki-yano/fuzzy-motion.vim",
+    config = function()
+      vim.keymap.set('n', 'f', '<cmd>FuzzyMotion<CR>')
+      vim.cmd("let g:fuzzy_motion_matchers = ['kensaku', 'fzf']")
+    end,
+    dependencies = {
+      "lambdalisue/kensaku.vim"
+    }
   },
 
   {
@@ -475,7 +490,10 @@ require("lazy").setup({
     lazy = false,
     config = function()
       vim.keymap.set('c', '<CR>', '<Plug>(kensaku-search-replace)<CR>')
-    end
+    end,
+    dependencies = {
+      "lambdalisue/kensaku.vim"
+    }
   },
   { 'lambdalisue/kensaku.vim', lazy = false },
 
