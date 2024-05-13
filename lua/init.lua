837ad56f60f26ccfeab63f74f40ada6582ac6095
shuntaka9576 <shuntaka9576@gmail.com>
2024-05-14 05:52:29 +0900

    Add nvim-cmp and nvim-metals

--- lua/init.lua
+++ lua/init.lua
@@ -420,15 +420,151 @@ require("lazy").setup({
     end,
   },
   {
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+      { "hrsh7th/cmp-nvim-lsp" },
+      { "hrsh7th/cmp-vsnip" },
+      { "hrsh7th/vim-vsnip" }
+    },
+    opts = function()
+      local cmp = require("cmp")
+      local conf = {
+        sources = {
+          { name = "nvim_lsp" },
+          { name = "vsnip" },
+        },
+        snippet = {
+          expand = function(args)
+            vim.fn["vsnip#anonymous"](args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<CR>"] = cmp.mapping.confirm({ select = true })
+        })
+      }
+      return conf
+    end
+  },
+  {
     "scalameta/nvim-metals",
     dependencies = {
       "nvim-lua/plenary.nvim",
+      {
+        "j-hui/fidget.nvim",
+        opts = {},
+      },
+      {
+        "mfussenegger/nvim-dap",
+        config = function(self, opts)
+          -- Debug settings if you're using nvim-dap
+          local dap = require("dap")
+
+          dap.configurations.scala = {
+            {
+              type = "scala",
+              request = "launch",
+              name = "RunOrTest",
+              metals = {
+                runType = "runOrTestFile",
+                --args = { "firstArg", "secondArg", "thirdArg" }, -- here just as an example
+              },
+            },
+            {
+              type = "scala",
+              request = "launch",
+              name = "Test Target",
+              metals = {
+                runType = "testTarget",
+              },
+            },
+          }
+        end
+      },
     },
     ft = { "scala", "sbt", "java" },
     opts = function()
       local metals_config = require("metals").bare_config()
+      metals_config.settings = {
+        showImplicitArguments = true,
+        excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
+      }
+      metals_config.init_options.statusBarProvider = "off"
+      metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
+
       metals_config.on_attach = function(client, bufnr)
-        -- your on_attach function
+        require("metals").setup_dap()
+
+        -- LSP mappings
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+        vim.keymap.set("n", "K", vim.lsp.buf.hover)
+        vim.keymap.set("n", "gi", vim.lsp.buf.implementation)
+        vim.keymap.set("n", "gr", vim.lsp.buf.references)
+        vim.keymap.set("n", "gds", vim.lsp.buf.document_symbol)
+        vim.keymap.set("n", "gws", vim.lsp.buf.workspace_symbol)
+        vim.keymap.set("n", "<leader>cl", vim.lsp.codelens.run)
+        vim.keymap.set("n", "<leader>sh", vim.lsp.buf.signature_help)
+        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename)
+        vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action)
+
+        vim.keymap.set("n", "<leader>ws", function()
+          require("metals").hover_worksheet()
+        end)
+
+        -- all workspace diagnostics
+        vim.keymap.set("n", "<leader>aa", vim.diagnostic.setqflist)
+
+        -- all workspace errors
+        vim.keymap.set("n", "<leader>ae", function()
+          vim.diagnostic.setqflist({ severity = "E" })
+        end)
+
+        -- all workspace warnings
+        vim.keymap.set("n", "<leader>aw", function()
+          vim.diagnostic.setqflist({ severity = "W" })
+        end)
+
+        -- buffer diagnostics only
+        vim.keymap.set("n", "<leader>d", vim.diagnostic.setloclist)
+
+        vim.keymap.set("n", "[c", function()
+          vim.diagnostic.goto_prev({ wrap = false })
+        end)
+
+        vim.keymap.set("n", "]c", function()
+          vim.diagnostic.goto_next({ wrap = false })
+        end)
+
+        -- Example mappings for usage with nvim-dap. If you don't use that, you can
+        -- skip these
+        vim.keymap.set("n", "<leader>dc", function()
+          require("dap").continue()
+        end)
+
+        vim.keymap.set("n", "<leader>dr", function()
+          require("dap").repl.toggle()
+        end)
+
+        vim.keymap.set("n", "<leader>dK", function()
+          require("dap.ui.widgets").hover()
+        end)
+
+        vim.keymap.set("n", "<leader>dt", function()
+          require("dap").toggle_breakpoint()
+        end)
+
+        vim.keymap.set("n", "<leader>dso", function()
+          require("dap").step_over()
+        end)
+
+        vim.keymap.set("n", "<leader>dsi", function()
+          require("dap").step_into()
+        end)
+
+        vim.keymap.set("n", "<leader>dl", function()
+          require("dap").run_last()
+        end)
       end
 
       return metals_config
@@ -443,5 +579,6 @@ require("lazy").setup({
         group = nvim_metals_group,
       })
     end
+
   }
 })
