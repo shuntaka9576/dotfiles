65804661db5004f5de76ca25a143300b8b0e3f52
shuntaka9576 <shuntaka9576@gmail.com>
2024-07-16 05:22:32 +0900

    Adjutment noice settings

--- lua/init.lua
+++ lua/init.lua
@@ -574,7 +574,13 @@ require("lazy").setup({
   {
     "folke/noice.nvim",
     event = "VeryLazy",
-    opts = {},
+    opts = {
+      lsp = {
+        progress = {
+          enabled = false,
+        },
+      },
+    },
     dependencies = {
       -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
       "MunifTanjim/nui.nvim",
