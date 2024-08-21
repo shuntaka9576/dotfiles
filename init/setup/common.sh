248e58c5db21580e4e707927a2fe517319d393b6
shuntaka9576 <shuntaka9576@gmail.com>
2024-08-22 04:35:54 +0900

    Format snippet.toml and adjutment cargo snippet

--- init/setup/common.sh
+++ init/setup/common.sh
@@ -58,6 +58,7 @@ cargo install stylua
 cargo install taplo-cli --locked
 cargo install cargo-edit
 cargo install cargo-watch
+cargo install sqlx-cli
 
 # install nerd-fonts
 if [ "$(uname)" == 'Darwin' ]  && [ "$(uname -m)" == 'arm64' ]; then
