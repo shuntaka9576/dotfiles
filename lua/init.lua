ac4186d2e73516497238f0a433f6d2f88b2e8a8f
shuntaka9576 <shuntaka9576@gmail.com>
2024-09-13 09:30:52 +0900

    Add flutter setting

--- lua/init.lua
+++ lua/init.lua
@@ -751,6 +751,76 @@ require("lazy").setup({
     "williamboman/mason-lspconfig.nvim",
   },
   {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "nvim-neotest/nvim-nio",
+      "rcarriga/nvim-dap-ui",
+    },
+    event = "VeryLazy",
+    config = function()
+      require("dapui").setup({
+        icons = { expanded = "▾", collapsed = "▸" },
+        layouts = {
+          {
+            elements = {
+              { id = "scopes", size = 0.25 },
+              "breakpoints",
+              "stacks",
+              "watches",
+            },
+            size = 10, -- columns
+            position = "bottom",
+          },
+        },
+      })
+    end,
+  },
+  -- flutter
+  {
+    "akinsho/flutter-tools.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "stevearc/dressing.nvim",
+    },
+    config = function()
+      require("flutter-tools").setup({
+        -- flutter_path = nil,
+        -- flutter_lookup_cmd = "asdf where flutter",
+        fvm = true,
+        widget_guides = { enabled = true },
+        lsp = {
+          settings = {
+            showtodos = true,
+            completefunctioncalls = true,
+            analysisexcludedfolders = {
+              vim.fn.expand("$Home/.pub-cache"),
+            },
+            renamefileswithclasses = "prompt",
+            updateimportsonrename = true,
+            enablesnippets = false,
+          },
+          on_attach = lsp_on_attach,
+        },
+        debugger = {
+          enabled = true,
+          run_via_dap = true,
+          exception_breakpoints = {},
+          register_configurations = function(paths)
+            local dap = require("dap")
+            dap.adapters.dart = {
+              type = "executable",
+              command = paths.flutter_bin,
+              args = { "debug-adapter" },
+            }
+            dap.configurations.dart = {}
+            require("dap.ext.vscode").load_launchjs()
+          end,
+        },
+      })
+    end,
+  },
+  {
     "neovim/nvim-lspconfig",
     config = function()
       local lspconfig = require("lspconfig")
