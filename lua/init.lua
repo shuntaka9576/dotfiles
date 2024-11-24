f74166b62ab7482928bab8542bd11145f2d72bb5
shuntaka9576 <shuntaka9576@gmail.com>
2024-11-25 04:56:17 +0900

    Delete plugins

--- lua/init.lua
+++ lua/init.lua
@@ -809,32 +809,6 @@ require("lazy").setup({
     "williamboman/mason-lspconfig.nvim",
   },
   {
-    "mfussenegger/nvim-dap",
-    dependencies = {
-      "nvim-neotest/nvim-nio",
-      "rcarriga/nvim-dap-ui",
-    },
-    event = "VeryLazy",
-    config = function()
-      require("dapui").setup({
-        icons = { expanded = "▾", collapsed = "▸" },
-        layouts = {
-          {
-            elements = {
-              { id = "scopes", size = 0.25 },
-              "breakpoints",
-              "stacks",
-              "watches",
-            },
-            size = 10, -- columns
-            position = "bottom",
-          },
-        },
-      })
-    end,
-  },
-  -- flutter
-  {
     "akinsho/flutter-tools.nvim",
     event = "VeryLazy",
     dependencies = {
