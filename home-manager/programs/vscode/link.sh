2517273fd1d184a58ad58c795a576075aaaa459e
shuntaka9576 <shuntaka9576@gmail.com>
2025-01-23 08:31:30 +0900

    Delete unused vscode settings

--- home-manager/programs/vscode/link.sh
+++ home-manager/programs/vscode/link.sh
@@ -2,5 +2,5 @@
 
 mkdir -p ~/Library/Application\ Support/Code/User
 
-ln -sf ~/dotfiles/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
-ln -sf ~/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+ln -sf ~/dotfiles/home-manager/programs/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
+ln -sf ~/dotfiles/home-manager/programs/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
