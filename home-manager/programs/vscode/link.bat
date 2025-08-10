ab35f8ae497dd25f4930967988d37f70d96adffd
shuntaka9576 <shuntaka9576@gmail.com>
2025-08-10 09:17:08 +0900

    Remove VSCode manual link scripts

--- home-manager/programs/vscode/link.bat
+++
@@ -1,5 +0,0 @@
-mklink %APPDATA%\Code\User\keybindings.json C:\dotfiles\vscode\keybindings.json
-mklink %APPDATA%\Code\User\settings.json C:\dotfiles\vscode\settings.json
-
-mklink %APPDATA%\"Code - Insiders"\User\keybindings.json C:\dotfiles\vscode\keybindings.json
-mklink %APPDATA%\"Code - Insiders"\User\settings.json C:\dotfiles\vscode\settings.json
--- home-manager/programs/vscode/link.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir -p ~/Library/Application\ Support/Code/User
-
-ln -sf ~/dotfiles/home-manager/programs/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
-ln -sf ~/dotfiles/home-manager/programs/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
