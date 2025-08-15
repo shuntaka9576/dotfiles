68ce80b7897366e2bb2d1b9792ba0ccd7cf7f7e1
shuntaka9576 <shuntaka9576@gmail.com>
2025-08-16 07:26:51 +0900

    style: format renovate.json and enable treefmt validation

--- treefmt.nix
+++ treefmt.nix
@@ -16,7 +16,6 @@
   settings.global.excludes = [
     "_sources/*"
     "home-manager/programs/nvim/lazy-lock.json"
-    "renovate.json"
   ];
   settings.formatter.shfmt.includes = [
     "*/zshrc"
