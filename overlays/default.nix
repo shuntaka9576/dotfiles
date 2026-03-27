1341bb3928c3ff9efc5f2471aed7f6cdae4ffa9f
shuntaka9576 <shuntaka9576@gmail.com>
2026-03-27 14:56:15 +0900

    chore(overlays): enable CGO for direnv build

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,4 +1,10 @@
 { self }:
 final: prev: {
   sources = final.callPackage "${self}/_sources/generated.nix" { };
+
+  direnv = prev.direnv.overrideAttrs (oldAttrs: {
+    env = (oldAttrs.env or { }) // {
+      CGO_ENABLED = "1";
+    };
+  });
 }
