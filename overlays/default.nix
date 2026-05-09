eb79b96ac71ad3237ec38871f420e2d915f11bff
shuntaka9576 <shuntaka9576@gmail.com>
2026-05-09 10:08:42 +0900

    chore: pin mise to separate nixpkgs in overlays

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,4 +1,8 @@
-{ self, nixpkgs-deno-pinned }:
+{
+  self,
+  nixpkgs-deno-pinned,
+  nixpkgs-mise-pinned,
+}:
 final: prev: {
   sources = final.callPackage "${self}/_sources/generated.nix" { };
 
@@ -9,6 +13,18 @@ final: prev: {
       })
     )
     deno
+    ;
+
+  # Pin mise to a separate nixpkgs to bypass our direnv overlay below.
+  # The overlay changes direnv's hash, which would otherwise force mise
+  # (which references direnv) to be rebuilt from source.
+  inherit
+    (
+      (import nixpkgs-mise-pinned {
+        inherit (final) system;
+      })
+    )
+    mise
     ;
 
   direnv = prev.direnv.overrideAttrs (oldAttrs: {
