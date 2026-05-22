7dffccd2e816d2ea7fe365905c57b0755533d99b
shuntaka9576 <12817245+shuntaka9576@users.noreply.github.com>
2026-05-22 20:34:51 +0000

    chore(deps): update nvfetcher sources

--- _sources/generated.nix
+++ _sources/generated.nix
@@ -98,13 +98,13 @@
   };
   worktrunk = {
     pname = "worktrunk";
-    version = "v0.52.0";
+    version = "v0.53.0";
     src = fetchFromGitHub {
       owner = "max-sixty";
       repo = "worktrunk";
-      rev = "v0.52.0";
+      rev = "v0.53.0";
       fetchSubmodules = false;
-      sha256 = "sha256-KWBN/y4SmS/T1D+/T7/GqlqXi+J/KyYAVzyarSZqbhc=";
+      sha256 = "sha256-1qWVELd9+XINjnzNNKeeCR9CxX/0DJVV6TS2cI0KhP4=";
     };
   };
   zenn-markdown-skill = {
